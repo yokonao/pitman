0c714a51705db79d8aae3fafc8a640fd095c5595
yokonao <nao-yokotsuka@c-fo.com>
2023-02-18 18:03:31 +0900

    PDF Spec のリンクを追加

--- character_set.go
+++ character_set.go
@@ -2,6 +2,7 @@ package main
 
 // This file is based on PDF Reference 1.7 7.2.2 "Character Set".
 // That chapter describes PDF character classification.
+// @see https://opensource.adobe.com/dc-acrobat-sdk-docs/pdfstandards/PDF32000_2008.pdf
 
 func isRegularChar(c byte) bool {
 	return !isDelemiterChar(c) && !isWhiteSpaceChar(c)
